9739869d995e555635edc7a66b6f0477b88b93ab
AugustineOchieng <gusochieng@gmail.com>
2019-06-03 05:08:29 +0300

    Register Business, Post and Profile class

--- neighbours/admin.py
+++ neighbours/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Neighbourhood
+from .models import Neighbourhood, Person, Business, Post
 
 # Register your models here.
 admin.site.register(Neighbourhood)
+admin.site.register(Person)
+admin.site.register(Business)
+admin.site.register(Post)
